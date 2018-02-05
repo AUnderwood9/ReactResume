37cef98e0c7018aa396b8789b3b8d105bcea11cf
AUnderwood9 <aunderwood797@gmail.com>
2018-02-05 17:02:18 -0600

    Added some more styling

--- src/components/ResumeContact.jsx
+++ src/components/ResumeContact.jsx
@@ -7,9 +7,12 @@ function ResumeContact (props) {
             <span className="left-contact-text">Lets Talk!</span>
             <span className="right-contact-text-top">Let's Work Together!</span>
             <span className="right-contact-text-btm">{props.resumeInfo[0].email}</span>
-            <input type="text"/>
-            <input type="text"/>
-            <input className="message-box" type="text"/>
+            <form action="">
+                <input type="text"/>
+                <input type="text"/>
+                <input className="message-box" type="text"/>
+            </form>
+            
             <div><button>Send</button></div>
             
         </section>
